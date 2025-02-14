607260271dfc829d5f77291c41a8dc6fd2dcc4a1
MartinKasperlik <martin.kasperlik@idc.com>
2025-02-14 16:42:41 +0100

    loading and saving the pretrained word embeddings into the project

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # IMPORTS:
+import gensim
+from gensim.models import KeyedVectors
 from function_definitions import *
 # -----------------------------------------------------------------------------------------------------
 
@@ -9,12 +11,16 @@ if __name__ == '__main__':
     user_name = get_local_username()
     input_file_name = "phrases"
     input_file_path = f"C:/Users/{user_name}/PycharmProjects/Galytix/{input_file_name}.xlsx"
+    embedding_vectors = "GoogleNews-vectors-negative300"
+    embedding_vectors_location = f"C:/Users/{user_name}/PycharmProjects/Galytix/{embedding_vectors}.bin"
 
-    # 2)
+    # 2) Loading the "phrases" file:
     df_raw = read_excel(input_file_path)
     print(df_raw)
 
-    # 3)
+    # 3) Loading the word embeddings for first million vectors
+    wv = KeyedVectors.load_word2vec_format(embedding_vectors_location, binary=True, limit=1000000)
+    wv.save_word2vec_format('vectors.csv')  # Saving the embeddings as a flatfile (csv)
 
 
 
--- requirements.md
+++ requirements.md
@@ -2,3 +2,4 @@ numpy==1.26.4
 pandas==2.2.3
 pytest==8.3.4
 openpyxl==3.1.5
+gensim==4.3.3
